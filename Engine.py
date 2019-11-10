daffc754ab522b90dac02694a49185e9d648259a
hewanguipath <he.wang@uipath.com>
2019-11-10 22:11:27 +1100

    stability improve

--- Engine.py
+++ Engine.py
@@ -8,10 +8,10 @@ gap1 = time.time()-start
 def move(fen_str):
     global engine
     board = chess.Board(fen_str)
-    result = engine.play(board, chess.engine.Limit(time=0.100))
+    result = engine.play(board, chess.engine.Limit(depth=15))
     gap2 = time.time()-start
     print(result.move)
-    return str(result.move) +" "+ str(gap1) +" " + str(gap2)
+    return str(result.move) +" "+ str(round(gap1,3)) +" " + str(round(gap2,3))
 
 if __name__ == "__main__":
-    print(move ("5k2/R7/2p2Pp1/1prp2Kp/p7/7P/6P1/8 w KQkq - 0 1"))
+    print(move ("r1b1k3/pppp3p/8/8/2P5/8/PP5P/RNBQK2R w KQkq - 0 1"))

f40a0c0125223e574dec7a190e20b34474c4505a
hewanguipath <he.wang@uipath.com>
2019-11-15 23:47:38 +1100

    1. Upgrade to latest version 2. Optimized with data scraping 3. Reduce Python reload 4. Search depth increase gradually, so move faster at begining

--- Engine.py
+++ Engine.py
@@ -2,16 +2,22 @@ import chess
 import chess.engine
 import time
 
-start=time.time()
+
 engine = chess.engine.SimpleEngine.popen_uci(r"C:\Users\He.Wang\rnd\stockfish-10-win\Windows\stockfish_10_x64_bmi2.exe")
-gap1 = time.time()-start
-def move(fen_str):
+
+def move(fen_str,move_steps=20):
     global engine
+    MAXSTEPS = 20
+    start=time.time()
+    if move_steps+5 >= MAXSTEPS:
+        depth = MAXSTEPS
+    else:
+        depth = move_steps+5
     board = chess.Board(fen_str)
-    result = engine.play(board, chess.engine.Limit(depth=15))
-    gap2 = time.time()-start
+    result = engine.play(board, chess.engine.Limit(depth=depth))
+    duration = time.time() - start
     print(result.move)
-    return str(result.move) +" "+ str(round(gap1,3)) +" " + str(round(gap2,3))
+    return str(result.move) +" "+ str(round(duration,3))
 
 if __name__ == "__main__":
-    print(move ("r1b1k3/pppp3p/8/8/2P5/8/PP5P/RNBQK2R w KQkq - 0 1"))
+    print(move ("r1b1k3/pppp3p/8/8/2P5/8/PP5P/RNBQK2R w KQkq - 0 1", 15))
